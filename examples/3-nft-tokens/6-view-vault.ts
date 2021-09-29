e3a2020b31039186841bf5614ac5364fbe8e079a
Monomadic <deathdisco@protonmail.com>
2021-09-29 16:49:45 +0800

    added basic builder for vault

--- examples/3-nft-tokens/6-view-vault.ts
+++ examples/3-nft-tokens/6-view-vault.ts
@@ -1,20 +1,19 @@
-import splToken = require('@solana/spl-token');
+// import splToken = require('@solana/spl-token');
+// import borsh = require('borsh');
 import web3 = require('@solana/web3.js');
-import borsh = require('borsh');
 
 // const vault = new web3.PublicKey("67TfEsvLRSmMYmf14rEX1VLGKYG9uuzdSgtTmF6bz4ae");
-const vault = new web3.PublicKey("6zTwh3MQK1h3mb2dM4deonby6kpE51yfrZKJa6gR8hGE");
-const connection = new web3.Connection(web3.clusterApiUrl("devnet"), "confirmed");
+const vault = new web3.PublicKey('6zTwh3MQK1h3mb2dM4deonby6kpE51yfrZKJa6gR8hGE');
+const connection = new web3.Connection(web3.clusterApiUrl('devnet'), 'confirmed');
 
 async function fetchAccountData(address: web3.PublicKey): Promise<any> {
-    return connection.getParsedAccountInfo(address).then(account => {
-		
+	return connection.getParsedAccountInfo(address).then((account) => {
 		console.log(account);
-        const data = account.value?.data as Buffer;
-		let key = data.slice(0,1);
+		const data = account.value?.data as Buffer;
+		let key = data.slice(0, 1);
 		console.log(key);
-        return data;
-    });
+		return data;
+	});
 }
 
 async function main() {
