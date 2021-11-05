9f9b209b638e8800655b988e0b5fe9dafcd492a6
Monomadic <deathdisco@protonmail.com>
2021-11-05 16:05:47 +0800

    cleared some cruft

---
+++ shared/mintNFT.ts
@@ -0,0 +1,151 @@
+import {
+  CreateAssociatedTokenAccount,
+  CreateMasterEdition,
+  CreateMetadata,
+  CreateMint,
+  Creator,
+  lookup,
+  MasterEdition,
+  Metadata,
+  MetadataDataData,
+  MintTo,
+  sendTransaction,
+  Wallet,
+} from '@metaplex/js';
+import { ASSOCIATED_TOKEN_PROGRAM_ID, MintLayout, Token, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { Connection, Keypair, PublicKey } from '@solana/web3.js';
+import BN from 'bn.js';
+
+interface MintNFTParams {
+	connection: Connection;
+	wallet: Wallet;
+	uri: string;
+	maxSupply: number;
+}
+
+interface MintNFTResponse {
+	txId: string;
+	mint: PublicKey;
+	metadata: PublicKey;
+	edition: PublicKey;
+}
+
+export const mintNFT = async ({
+	connection,
+	wallet,
+	uri,
+	maxSupply,
+}: MintNFTParams): Promise<MintNFTResponse> => {
+	const mint = Keypair.generate();
+
+	const metadataPDA = await Metadata.getPDA(mint.publicKey);
+	const editionPDA = await MasterEdition.getPDA(mint.publicKey);
+
+	const mintRent = await connection.getMinimumBalanceForRentExemption(MintLayout.span);
+
+	const {
+		name,
+		symbol,
+		seller_fee_basis_points,
+		properties: { creators },
+	} = await lookup(uri);
+
+	const creatorsData = creators.reduce<Creator[]>((memo, { address, share }) => {
+		const verified = address === wallet.publicKey.toString();
+
+		const creator = new Creator({
+			address,
+			share,
+			verified,
+		});
+
+		memo = [...memo, creator];
+
+		return memo;
+	}, []);
+
+	const createMintTx = new CreateMint(
+		{ feePayer: wallet.publicKey },
+		{
+			newAccountPubkey: mint.publicKey,
+			lamports: mintRent,
+		}
+	);
+
+	const metadataData = new MetadataDataData({
+		name,
+		symbol,
+		uri,
+		sellerFeeBasisPoints: seller_fee_basis_points,
+		creators: creatorsData,
+	});
+
+	const createMetadataTx = new CreateMetadata(
+		{
+			feePayer: wallet.publicKey,
+		},
+		{
+			metadata: metadataPDA,
+			metadataData,
+			updateAuthority: wallet.publicKey,
+			mint: mint.publicKey,
+			mintAuthority: wallet.publicKey,
+		}
+	);
+
+	const recipient = await Token.getAssociatedTokenAddress(
+		ASSOCIATED_TOKEN_PROGRAM_ID,
+		TOKEN_PROGRAM_ID,
+		mint.publicKey,
+		wallet.publicKey
+	);
+
+	const createAssociatedTokenAccountTx = new CreateAssociatedTokenAccount(
+		{ feePayer: wallet.publicKey },
+		{
+			associatedTokenAddress: recipient,
+			splTokenMintAddress: mint.publicKey,
+		}
+	);
+
+	const mintToTx = new MintTo(
+		{ feePayer: wallet.publicKey },
+		{
+			mint: mint.publicKey,
+			dest: recipient,
+			amount: 1,
+		}
+	);
+
+	const masterEditionTx = new CreateMasterEdition(
+		{ feePayer: wallet.publicKey },
+		{
+			edition: editionPDA,
+			metadata: metadataPDA,
+			updateAuthority: wallet.publicKey,
+			mint: mint.publicKey,
+			mintAuthority: wallet.publicKey,
+			maxSupply: new BN(maxSupply),
+		}
+	);
+
+	const txId = await sendTransaction({
+		connection,
+		signers: [mint],
+		txs: [
+			createMintTx,
+			createMetadataTx,
+			createAssociatedTokenAccountTx,
+			mintToTx,
+			masterEditionTx,
+		],
+		wallet,
+	});
+
+	return {
+		txId,
+		mint: mint.publicKey,
+		metadata: metadataPDA,
+		edition: editionPDA,
+	};
+};
